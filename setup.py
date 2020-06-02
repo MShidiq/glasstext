27187d9b24985dc6fe2fb40ce099568eab35137c
MShidiq <mshidiq98@gmail.com>
2020-06-02 10:21:23 +0700

    updating module & adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='glasstext',
+   version='0.2',
+   description='python module for making invisible text',
+   author='Maulana Shidiq',
+   author_email='mshidiq98@gmail.com',
+   packages=['glasstext'],
+)
